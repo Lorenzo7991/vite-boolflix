1c8aa59aa4df1a6b5f14c06d30cc6eedf14fe4f3
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-02-07 19:11:09 +0100

    add flags logic, tv shows list on page

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ import AppMain from './components/AppMain.vue';
 export default {
   name: 'App',
   data: () => ({
-    movieResults: []
+    movieResults: [],
+    tvResults: [],
   }),
   components: { AppHeader, AppMain },
   methods: {
@@ -22,17 +23,34 @@ export default {
           this.movieResults = response.data.results;
         })
         .catch(error => {
-          console.log('API call error:', error);
+          console.log('API call error for movies:', error);
+        });
+
+      axios
+        .get('https://api.themoviedb.org/3/search/tv', {
+          params: {
+            api_key: 'a3cc6271bb5f76a0046a2b13e276b54c',
+            query: searchQuery
+          }
+        })
+        .then(response => {
+          this.tvResults = response.data.results;
+        })
+        .catch(error => {
+          console.log('API call error for TV shows:', error);
         });
     }
   }
 };
 </script>
 
+
 <template>
   <AppHeader @search="handleSearch" />
-  <AppMain :movieResults="movieResults" />
+  <AppMain :movieResults="movieResults" :tvResults="tvResults" />
 </template>
+
+
 <style lang="scss">
 @use './assets/scss/style.scss';
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,27 +5,66 @@ export default {
         movieResults: {
             type: Array,
 
-        }
+        },
+
+        tvResults: {
+            type: Array
+        },
+
     }
 };
 </script>
 
 
 <template>
-    <div class="text-white">
+    <div id="movies" class="text-white">
         <h2>Movie Results:</h2>
         <ul>
             <li class="border-bottom border-4 mb-4" v-for="movie in movieResults" :key="movie.id">
                 <div><strong>Title:</strong> {{ movie.title }}</div>
                 <div><strong>Original Title:</strong> {{ movie.original_title }}</div>
-                <div><strong>Language:</strong> {{ movie.original_language }}</div>
+                <div id="ita-flag" v-if="movie.original_language === 'it'">
+                    <img src="@/assets/img/it.png" alt="Italian Flag Image">
+                </div>
+                <div id="eng-flag" v-else-if="movie.original_language === 'en'">
+                    <img src="@/assets/img/en.png" alt="English Flag Image">
+                </div>
+                <div v-else>
+                    <span><i class="fa-solid fa-language fa-2x"></i></span>
+                </div>
                 <div><strong>Vote Average:</strong> {{ movie.vote_average }}</div>
+            </li>
+        </ul>
+    </div>
+    <div id="tv-series" class="text-white">
+        <h2>TV Show Results:</h2>
+        <ul>
+            <li class="border-bottom border-4 mb-4" v-for="show in tvResults" :key="show.id">
+                <div><strong>Title:</strong> {{ show.name }}</div>
+                <div><strong>Original Title:</strong> {{ show.original_name }}</div>
+                <div id="ita-flag" v-if="show.original_language === 'it'">
+                    <img src="@/assets/img/it.png" alt="Italian Flag Image">
+                </div>
+                <div id="eng-flag" v-else-if="show.original_language === 'en'">
+                    <img src="@/assets/img/en.png" alt="English Flag Image">
+                </div>
+                <div v-else>
+                    <span><i class="fa-solid fa-language fa-2x"></i></span>
+                </div>
+                <div><strong>Vote Average:</strong> {{ show.vote_average }}</div>
             </li>
         </ul>
     </div>
 </template>
 
-
 <style scoped>
-/* Style here */
+#ita-flag img {
+    max-height: 20px;
+    max-width: 50px;
+}
+
+#eng-flag img {
+    max-height: 20px;
+    max-width: 50px;
+}
 </style>
