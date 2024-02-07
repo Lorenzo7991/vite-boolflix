3d049b08580be9d0320212014d372e84edacb0b8
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-02-07 19:13:29 +0100

    add main container

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -17,43 +17,46 @@ export default {
 
 
 <template>
-    <div id="movies" class="text-white">
-        <h2>Movie Results:</h2>
-        <ul>
-            <li class="border-bottom border-4 mb-4" v-for="movie in movieResults" :key="movie.id">
-                <div><strong>Title:</strong> {{ movie.title }}</div>
-                <div><strong>Original Title:</strong> {{ movie.original_title }}</div>
-                <div id="ita-flag" v-if="movie.original_language === 'it'">
-                    <img src="@/assets/img/it.png" alt="Italian Flag Image">
-                </div>
-                <div id="eng-flag" v-else-if="movie.original_language === 'en'">
-                    <img src="@/assets/img/en.png" alt="English Flag Image">
-                </div>
-                <div v-else>
-                    <span><i class="fa-solid fa-language fa-2x"></i></span>
-                </div>
-                <div><strong>Vote Average:</strong> {{ movie.vote_average }}</div>
-            </li>
-        </ul>
-    </div>
-    <div id="tv-series" class="text-white">
-        <h2>TV Show Results:</h2>
-        <ul>
-            <li class="border-bottom border-4 mb-4" v-for="show in tvResults" :key="show.id">
-                <div><strong>Title:</strong> {{ show.name }}</div>
-                <div><strong>Original Title:</strong> {{ show.original_name }}</div>
-                <div id="ita-flag" v-if="show.original_language === 'it'">
-                    <img src="@/assets/img/it.png" alt="Italian Flag Image">
-                </div>
-                <div id="eng-flag" v-else-if="show.original_language === 'en'">
-                    <img src="@/assets/img/en.png" alt="English Flag Image">
-                </div>
-                <div v-else>
-                    <span><i class="fa-solid fa-language fa-2x"></i></span>
-                </div>
-                <div><strong>Vote Average:</strong> {{ show.vote_average }}</div>
-            </li>
-        </ul>
+    <div id="temporary-container" class="container">
+
+        <div id="movies" class="text-white my-5">
+            <h2>Movie Results:</h2>
+            <ul>
+                <li class="border-bottom border-4 mb-4" v-for="movie in movieResults" :key="movie.id">
+                    <div><strong>Title:</strong> {{ movie.title }}</div>
+                    <div><strong>Original Title:</strong> {{ movie.original_title }}</div>
+                    <div id="ita-flag" v-if="movie.original_language === 'it'">
+                        <img src="@/assets/img/it.png" alt="Italian Flag Image">
+                    </div>
+                    <div id="eng-flag" v-else-if="movie.original_language === 'en'">
+                        <img src="@/assets/img/en.png" alt="English Flag Image">
+                    </div>
+                    <div v-else>
+                        <span><i class="fa-solid fa-language fa-2x"></i></span>
+                    </div>
+                    <div><strong>Vote Average:</strong> {{ movie.vote_average }}</div>
+                </li>
+            </ul>
+        </div>
+        <div id="tv-series" class="text-white">
+            <h2>TV Show Results:</h2>
+            <ul>
+                <li class="border-bottom border-4 mb-4" v-for="show in tvResults" :key="show.id">
+                    <div><strong>Title:</strong> {{ show.name }}</div>
+                    <div><strong>Original Title:</strong> {{ show.original_name }}</div>
+                    <div id="ita-flag" v-if="show.original_language === 'it'">
+                        <img src="@/assets/img/it.png" alt="Italian Flag Image">
+                    </div>
+                    <div id="eng-flag" v-else-if="show.original_language === 'en'">
+                        <img src="@/assets/img/en.png" alt="English Flag Image">
+                    </div>
+                    <div v-else>
+                        <span><i class="fa-solid fa-language fa-2x"></i></span>
+                    </div>
+                    <div><strong>Vote Average:</strong> {{ show.vote_average }}</div>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
