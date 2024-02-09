670d9c97145d736b1db68e042e1d418dc9ebc14c
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-02-09 17:44:21 +0100

    add movie component draft layout

--- src/components/main-components/MovieResults.vue
+++ src/components/main-components/MovieResults.vue
@@ -12,29 +12,57 @@ export default {
 
 <template>
     <!-- Movie Results Section -->
-    <div id="movies" class="text-white my-5 " v-if="movieResults.length > 0">
+    <div id="movies" class="text-white text-center my-5" v-if="movieResults.length > 0">
         <h2>Movie Results:</h2>
-        <ul>
+        <div class="movies-grid">
             <!-- Loop through movie results -->
-            <li class="border-bottom border-4 mb-4" v-for="movie in movieResults" :key="movie.id">
-                <div><strong>Title:</strong> {{ movie.title }}</div>
-                <div><strong>Original Title:</strong> {{ movie.original_title }}</div>
-                <!-- Display Italian flag if original language is Italian -->
-                <div id="ita-flag" v-if="movie.original_language === 'it'">
-                    <img src="@/assets/img/it.png" alt="Italian Flag Image">
+            <div class="movie-item" v-for="movie in movieResults" :key="movie.id">
+                <div class="poster" v-bind:style="{ 'background-image': `url(${basePosterPath}${movie.poster_path})` }">
+                    <div><strong>Title:</strong> {{ movie.title }}</div>
+                    <div><strong>Original Title:</strong> {{ movie.original_title }}</div>
+                    <!-- Display Italian flag if original language is Italian -->
+                    <div class="flag" v-if="movie.original_language === 'it'">
+                        <img src="@/assets/img/it.png" alt="Italian Flag Image">
+                    </div>
+                    <!-- Display English flag if original language is English -->
+                    <div class="flag" v-else-if="movie.original_language === 'en'">
+                        <img src="@/assets/img/en.png" alt="English Flag Image">
+                    </div>
+                    <div v-else>
+                        <span><strong>Language:</strong><br />{{ movie.original_language }}</span>
+                    </div>
+                    <div><strong>Vote Average:</strong><br /> <span v-html="transformVote(movie.vote_average)"></span>
+                    </div>
                 </div>
-                <!-- Display English flag if original language is English -->
-                <div id="eng-flag" v-else-if="movie.original_language === 'en'">
-                    <img src="@/assets/img/en.png" alt="English Flag Image">
-                </div>
-                <div v-else>
-                    <span><strong>Language:</strong> {{ movie.original_language }}</span>
-                </div>
-                <img v-if="movie.poster_path" :src="`${basePosterPath}${movie.poster_path}`" alt="Movie Poster">
-                <!-- Display placeholder image if poster_path is not present -->
-                <img v-else src="@/assets/img/placeholder.jpg" alt="Placeholder Image">
-                <div><strong>Vote Average:</strong> <span v-html="transformVote(movie.vote_average)"></span></div>
-            </li>
-        </ul>
+            </div>
+        </div>
     </div>
 </template>
+  
+<style scoped>
+.movies-grid {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+}
+
+.movie-item {
+    width: calc(25% - 20px);
+    margin: 10px;
+    border: 1px solid #ccc;
+}
+
+.flag img {
+    max-width: 20px;
+}
+
+.poster {
+    width: 100%;
+    height: 400px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+
+}
+</style>
+  
--- src/components/main-components/TvShowsResults.vue
+++ src/components/main-components/TvShowsResults.vue
@@ -40,6 +40,3 @@ export default {
 </template>
 
 
-<style scoped>
-/* Style here */
-</style>
